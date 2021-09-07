6d9bb0925873222af4a42c831a9944dbc779b2b5
JuanSebastianGB <3393@holbertonschool.com>
2021-09-07 03:32:14 +0000

    UPL: Uploading files to check is a node has a cycle, thi include the header and the function

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Function used to check if it is a cycle
+ * @list: Structure of type listint_t which content the node to check
+ * Return:
+ *        1 if has cycle
+ *        0 if has not cycle
+ **/
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *quick;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	slow = list->next;
+	quick = slow->next;
+
+	while (quick != NULL)
+	{
+		if (quick == slow)
+			return (1);
+		slow = slow->next;
+		quick = quick->next->next;
+	}
+	return (0);
+}
