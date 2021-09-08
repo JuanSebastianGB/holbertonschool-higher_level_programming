1a82dd90fd7ff9f904fd7ea20d1ee735b268016a
JuanSebastianGB <3393@holbertonschool.com>
2021-09-08 21:50:32 +0000

    UPD: Solving requirementn when not arguments print .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    longitud = len(argv) - 1
-    print("{:d} {:s}:".format(
-        longitud, "arguments" if longitud > 2 else "argument"))
+    lgth = len(argv) - 1
+    print("{:d} {:s}{:s}".format(
+        lgth, "arguments" if lgth > 2 else "argument",
+        "." if lgth == 1 else ":"))
     for i, s in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, s))
