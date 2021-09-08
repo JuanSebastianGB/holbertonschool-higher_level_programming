31eba794ec9d4b997d8fed602eaa922b2ed1cb8e
JuanSebastianGB <3393@holbertonschool.com>
2021-09-08 20:08:08 +0000

    FIX: Completing the required string

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     longitud = len(argv) - 1
-    print("{:d}".format(longitud))
+    print("{:d} arguments:".format(longitud))
     for i in range(1, longitud + 1):
         print("{:d}: {:s}".format(i, argv[i]))
