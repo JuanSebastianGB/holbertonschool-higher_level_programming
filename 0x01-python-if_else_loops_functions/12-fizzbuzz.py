db9a57b2b212a5c355b417450d8046cd28613241
JuanSebastianGB <3393@holbertonschool.com>
2021-09-08 03:38:13 +0000

    FIX: Implementing logic corrections

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("{:s} ".format("FizzBuzz"), end="")
-    elif i % 3 == 0:
-        print("{:s} ".format("Fizz"), end="")
-    elif i % 5 == 0:
-        print("{:s} ".format("Buzz"), end="")
-    else:
-        print("{:d} ".format(i), end="")
-print("")
+def fizzbuzz():
+
+    for i in range(1, 101):
+        if i % 3 != 0 and i % 5 != 0:
+            print("{:d}".format(i), end="")
+        if i % 3 == 0:
+            print("Fizz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        print(end=" ")
