50c34e5285c8aa8743aa6cf342e5648842fc5a75
JuanSebastianGB <3393@holbertonschool.com>
2021-09-08 20:42:56 +0000

    UPD: Implementing built in funciton enumerate  instead of range in loop for

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     longitud = len(argv) - 1
     print("{:d} arguments:".format(longitud))
-    for i in range(1, longitud + 1):
-        print("{:d}: {:s}".format(i, argv[i]))
+    for i, s in enumerate(argv):
+        if i > 0:
+            print("{:d}: {:s}".format(i, s))
