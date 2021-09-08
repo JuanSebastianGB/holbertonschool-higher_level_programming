d10d501b89a8ea83389e5896936969912c50cf3f
JuanSebastianGB <3393@holbertonschool.com>
2021-09-08 20:46:26 +0000

    UPD:Implemetning ternary conditional to handle cases when there is only one argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 if __name__ == "__main__":
     from sys import argv
     longitud = len(argv) - 1
-    print("{:d} arguments:".format(longitud))
+    print("{:d} {:s}:".format(
+        longitud, "arguments" if longitud > 2 else "argument"))
     for i, s in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, s))
