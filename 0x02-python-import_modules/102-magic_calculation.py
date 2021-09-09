b3b5efa4cf53df3f038f6ffdb0e51e3c8724b947
JuanSebastianGB <3393@holbertonschool.com>
2021-09-09 03:55:50 +0000

    UPD: Removing blank spaces

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """[Implementing function from assembly code]
 """
-
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-
     if a < b:
         c = add(a, b)
     for i in range(4, 6):
