3c9299fb83c1ea385faaf265d72057a0cab8f0b3
JuanSebastianGB <3393@holbertonschool.com>
2021-09-09 01:46:35 +0000

    UPL: Uploading file to read

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """[working whith file like an object]
+    """
     import hidden_4
     for text in dir(hidden_4):
         if text[:2] != "__":
