4448962c624d77aaa9c6c92fbb6968bb1b706c0f
JuanSebastianGB <3393@holbertonschool.com>
2021-09-09 04:17:22 +0000

    FIX: correcting wrong position of for loop and adding commentary descriptions

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+    """[Emulating the output in assembly required]
+
+    Args:
+        a ([int]): [number 1]
+        b ([int]): [number 2]
+
+    Returns:
+        [int]: [result]
+    """
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-    for i in range(4, 6):
-        c = add(c, i)
+        for i in range(4, 6):
+            c = add(c, i)
         return c
     else:
-        return sub(a, b)
+        return (sub(a, b))
