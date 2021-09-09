df588e43a42d982e74dd18d25f2d2b71ec9671c6
JuanSebastianGB <3393@holbertonschool.com>
2021-09-09 01:36:43 +0000

    STYLE: Performing less lines of code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,6 @@ if __name__ == "__main__":
     """[Printing all file names]
     """
     import hidden_4
-    files = dir(hidden_4)  # Accesing to all propertys of the file hiddden_4
-    for file in files:  # Handeling like an object
-        if file[:2] != "__":  # printing all different of attributtes
-            print(file)
+    for text in dir(hidden_4):  # Handeling like an object
+        if text[:2] != "__":  # printing all different of attributtes
+            print("{}".format(text))
