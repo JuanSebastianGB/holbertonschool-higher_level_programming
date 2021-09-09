af2e7290f7ea4883049ae450dbc442878b014fe6
JuanSebastianGB <3393@holbertonschool.com>
2021-09-09 01:44:03 +0000

    UPD: Removing lines to test

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """[Printing all file names]
-    """
     import hidden_4
-    for text in dir(hidden_4):  # Handeling like an object
-        if text[:2] != "__":  # printing all different of attributtes
-            print("{}".format(text))
+    for text in dir(hidden_4):
+        if text[:2] != "__":
+            print("{:s}".format(text))
