4df2967c58e9f0fc4464653a3ace7b0d998d811e
JuanSebastianGB <3393@holbertonschool.com>
2021-09-14 00:21:27 +0000

    UPL: Uploading dir of 0x03 and the first point developed

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """[function that prints all integers of a list.]
+
+    Args:
+        my_list (list, optional): [list of elements]. Defaults to [].
+    """
+    for element in my_list[::-1]:
+        print(element)
