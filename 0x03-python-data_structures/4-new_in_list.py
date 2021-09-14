70c2198b921d5fc1da869ec1e71e14c93f1e26fa
JuanSebastianGB <3393@holbertonschool.com>
2021-09-14 01:28:39 +0000

    FIX: Changing or by and in the condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     """replaces an element in a list at a specific position
     without modifying the original list (like in C)."""
     copy = my_list[:]
-    if idx >= 0 or idx + 1 <= len(my_list):
+    if idx >= 0 and idx + 1 <= len(my_list):
         copy[idx] = element
     return copy
