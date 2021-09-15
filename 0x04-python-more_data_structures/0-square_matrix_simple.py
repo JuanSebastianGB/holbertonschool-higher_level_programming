7782462493fb01cd19745565f887b77b7886049b
JuanSebastianGB <3393@holbertonschool.com>
2021-09-15 17:32:59 +0000

    UPD: Updating function to use a function in each row, implementing map and lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     """computes the square value of all integers of a matrix."""
-    return [[el[0]**2, el[1]**2, el[2]**2] for el in matrix]
+    return [list(map(lambda x: x**2, el)) for el in matrix]
