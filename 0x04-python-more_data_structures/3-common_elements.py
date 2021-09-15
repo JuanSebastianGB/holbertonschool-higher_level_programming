cc97fd7e74a3b86ad4a7fcdd5f6c3a5a6d75b10d
JuanSebastianGB <3393@holbertonschool.com>
2021-09-15 19:26:06 +0000

    UPD: Improving algorythm, implmenting & union operation, because thera being implemented sets of data

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     """returns a set of common elements in two sets."""
-    listado = []
-    for el in set_1:
-        if el in set_2:
-            listado.append(el)
-    return listado
+    return set_1 & set_2
