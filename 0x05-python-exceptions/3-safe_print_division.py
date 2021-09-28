cf35a773292571e9c30bf54fa5c536698f7aabc9
JuanSebastianGB <3393@holbertonschool.com>
2021-09-28 03:12:58 +0000

    FIX: Removing typo condition to test

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    """Return divition of a with b"""
     try:
         result = a / b
-    except (ZeroDivisionError, TypeError):
+    except ZeroDivisionError:
         result = None
     finally:
         print("inside result: {}".format(result))
