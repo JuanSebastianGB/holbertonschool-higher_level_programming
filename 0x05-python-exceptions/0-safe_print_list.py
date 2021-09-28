e81acad1556b34867389a7af85176cbb16da51cc
JuanSebastianGB <3393@holbertonschool.com>
2021-09-28 02:17:48 +0000

    UPL: Uploading task0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    """function that prints x elements of a list. using exception"""
+    counter = 0
+    for index in range(x):
+        try:
+            print("{}".format(my_list[index]), end="")
+            counter += 1
+        except IndexError:
+            break
+    print("")
+    return counter
