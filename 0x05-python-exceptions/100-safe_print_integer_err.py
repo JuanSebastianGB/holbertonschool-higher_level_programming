3c50c97f4b0a57265fdce90508244c310b0375fb
JuanSebastianGB <3393@holbertonschool.com>
2021-09-28 04:04:10 +0000

    UPL: Uploading task number 7, implmeneting exception in the printing of a numbert, taking in account a stderror in case is preesent an error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    """prints an integer."""
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as errors:
+        from sys import stderr
+        print("Exception: {}".format(errors))
+        return False    
+    else:
+        return True
