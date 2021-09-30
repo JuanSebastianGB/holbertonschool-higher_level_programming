95e4c475b11ed7188206eea87698b8c6510e9590
JuanSebastianGB <3393@holbertonschool.com>
2021-09-30 04:41:58 +0000

    UPL: Uploading Task numer 9, Implementing magic methods for comparation

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Defining the class square"""
+
+
+class Square:
+    """Representing a square"""
+
+    def __init__(self, size=0):
+        """Constructing a square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = size
+
+    @property  # getter
+    def size(self):
+        """Getter of the size of the square"""
+        return self.__size
+
+    @size.setter  # setter
+    def size(self, value):
+        """Setter of the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Cumputes area of the square"""
+        return self.size ** 2
+
+    def __eq__(self, o: object) -> bool:
+        """Implementing =="""
+        return self.size == o.size
+
+    def __ne__(self, o: object) -> bool:
+        """Implementing !="""
+        return self.size != o.size
+
+    def __gt__(self, o: object) -> bool:
+        """Implementing >"""
+        return self.size > o.size
+
+    def __lt__(self, o: object) -> bool:
+        """Implementing <"""
+        return self.size < o.size
+
+    def __ge__(self, o: object) -> bool:
+        """Implementing >="""
+        return self.size >= o.size
+
+    def __le__(self, o: object) -> bool:
+        """Implementing <="""
+        return self.size <= o.size
