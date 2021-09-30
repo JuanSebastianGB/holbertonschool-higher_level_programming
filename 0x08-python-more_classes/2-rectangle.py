d0f0a1bd1f0f6ccda8e97420b4f05554b37a6087
JuanSebastianGB <3393@holbertonschool.com>
2021-09-30 01:28:46 +0000

    UPL: Uploading Task number 2, adding functiont area ant perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Defining class Rectangle"""
+
+
+class Rectangle:
+    """Representing a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Constructing the rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Setter of width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Setter of height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Getter of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Getter of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Compute the area of the rectangle"""
+        return self.__height*self.__width
+
+    def perimeter(self):
+        """Compute the perimeter of the rectangle"""
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return 2 * (self.__height + self.__width)
