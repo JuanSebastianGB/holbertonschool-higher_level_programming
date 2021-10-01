c3ec80715a78798eb0a5bd3c30229d7a79897048
JuanSebastianGB <3393@holbertonschool.com>
2021-10-01 03:08:30 +0000

    DEL: Deleting unnecessary text test

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""[Defining the locked Class]
-"""
+"""Defining the locked Class"""
+
 
 class LockedClass:
-    """[prevents the user from dynamically creating new instance
-    attributes, except if the new instance attribute is
-    called first_name.]
-    """
+    """prevents the user from dynamically creating new instance"""
 
     __slots__ = ["first_name"]
