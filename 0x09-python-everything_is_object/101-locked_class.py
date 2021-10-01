b51cfde3e855cf18a17ce0e50e26df3f04d3011b
JuanSebastianGB <3393@holbertonschool.com>
2021-10-01 03:04:11 +0000

    FIX: Fixing the name used with slots

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,11 +2,10 @@
 """[Defining the locked Class]
 """
 
-
 class LockedClass:
     """[prevents the user from dynamically creating new instance
-    attributes, except if the new instance attribute is 
+    attributes, except if the new instance attribute is
     called first_name.]
     """
 
-    __slots__ = ["first name"]
+    __slots__ = ["first_name"]
