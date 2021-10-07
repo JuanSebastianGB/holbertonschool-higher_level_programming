d28557f35852a906ed897c630462988371dde6bd
JuanSebastianGB <3393@holbertonschool.com>
2021-10-07 02:34:24 +0000

    UPD: Updting Task 4, implementing doc and it's respectives tests

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+
+""" This Defines the print_square function"""
+
+
 def print_square(size):
+    """[Print a matrix of #, depending on the input size]
+
+    Args:
+        size ([int]): [size of the square]
+
+    Raises:
+        TypeError: [if size is not a valid number]
+        ValueError: [if size is less than 0]
+    """
 
     if type(size) is not int:
         raise TypeError("size must be an integer")
@@ -7,7 +20,9 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if type(size) is float and size < 0:
         raise ValueError("size must be an integer")
+    if size == 0:
+        return
 
     string = "#" * size + "\n"
     string *= size
-    print(string)
+    print(string[:-1])
