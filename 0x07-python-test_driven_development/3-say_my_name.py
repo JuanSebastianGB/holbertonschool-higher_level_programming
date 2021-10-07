e9fc2008af8b6a4291453110b0996de16ffce8f9
JuanSebastianGB <3393@holbertonschool.com>
2021-10-07 01:56:25 +0000

    UPD: IMplementing docs and tests for task 3, sy my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-def say_my_name(first_name, last_name=""):
+""" Module that says a name with a last name"""
 
-    if type(first_name) is str:
+
+def say_my_name(first_name, last_name=""):
+    """[Function that say the name with the input strings]
+
+    Args:
+        first_name ([str]): [First name, which is mandatory]
+        last_name (str, optional): [Last name that can be gived or not].
+        Defaults to "".
+
+    Raises:
+        TypeError: [If the name gived not is a string]
+        TypeError: [If the last name gived not is a string]
+    """
+
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
 
-    if type(last_name) is str:
-        raise TypeError("last_name must be a string must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
