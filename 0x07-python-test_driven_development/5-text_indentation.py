4afcfd5cb18d365492e1ae569c6478e1f46362ad
JuanSebastianGB <3393@holbertonschool.com>
2021-10-07 03:50:19 +0000

    UPD: Upating files of task 5, implementing doc and tests

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-from typing import Type
+""" Definition of the function text_indentation """
 
 
 def text_indentation(text):
+    """[ prints a text with 2 new lines after each of these characters: ., ? and :]
+
+    Args:
+        text ([str]): [text - string to process]
+
+    Raises:
+        TypeError: [if not is a string]
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    text = text.replace(".", ".\n\n").replace(
-        "?", "?\n\n").replace(":", ":\n\n")
-    print(text)
+    text = text.replace(". ", ".\n\n").replace(
+        "? ", "?\n\n").replace(": ", ":\n\n")
+    text = "\n".join([row.lstrip(" ").rstrip(" ") for row in text.split("\n")])
+    print(text, end="")
