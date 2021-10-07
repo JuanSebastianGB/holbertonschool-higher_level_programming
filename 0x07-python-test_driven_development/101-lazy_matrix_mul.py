f19e6ec8395e10194738abcbf4d310c364bef97b
JuanSebastianGB <3393@holbertonschool.com>
2021-10-07 04:54:16 +0000

    UPL: Uploading file of the advanced tas 101-lazy-mul

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defines lazy_matrix_mul function"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Return the result of mult. between 2 matrix"""
+    return numpy.matmul(m_a, m_b)
