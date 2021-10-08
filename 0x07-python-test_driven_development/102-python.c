addb9da2513515664860d25acbb980a3324b47fe
JuanSebastianGB <3393@holbertonschool.com>
2021-10-08 04:32:30 +0000

    UPD: testing some cases

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -9,24 +9,22 @@ void print_python_string(PyObject *p)
 {
 	Py_ssize_t length;
 
-	(void)p;
-	(void)length;
 	fflush(stdout);
 
 	printf("[.] string object info \n");
 /* Checking invalid string*/
 	if (strcmp("str", p->ob_type->tp_name))
 	{
-		printf(" [ERROR] Invalid String Object\n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 	length = ((PyASCIIObject *)(p))->length;
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf(" type: compact ascii\n");
+		printf("  type: compact ascii\n");
 	else
 	{
-		printf(" type: compact unicode object\n");
+		printf("  type: compact unicode object\n");
 	}
-	printf(" length: %li\n", length);
-	printf(" value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+	printf("  length: %li\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
