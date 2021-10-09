32c279e05c810d4d8226e7757b8f4699c3e725b8
JuanSebastianGB <3393@holbertonschool.com>
2021-10-09 01:01:30 +0000

    UPD: CHanging 'Python.h by <Python.h>' like header file include

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,17 +1,18 @@
-#include "Python.h"
+#include <Python.h>
 
 
 /**
  * print_python_string - prints Python strings.
  * @p: PyObject expected string
  */
+
 void print_python_string(PyObject *p)
 {
 	Py_ssize_t length;
 
 	fflush(stdout);
 
-	printf("[.] string object info \n");
+	printf("[.] string object info\n");
 /* Checking invalid string*/
 	if (strcmp("str", p->ob_type->tp_name))
 	{
