e2c3d0d3da85d753ec28659ecf7b541aba4a78f9
JuanSebastianGB <3393@holbertonschool.com>
2021-10-11 16:29:48 +0000

    UPL: Uploading Task 6, to implement an exception in the class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """Defining Area"""
+    pass
+
+    def area(self):
+        """ Instance method that raise an exception"""
+        raise Exception("area() is not implemented")
