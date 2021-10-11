cd6da0356bbae6e32f41ef1d73e098c0ce3b836f
JuanSebastianGB <3393@holbertonschool.com>
2021-10-11 12:51:17 +0000

    STYLE: Accomplish pip8

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    """ Function that returnn all the information of the object like list of objects"""
+    """ Function that returnn all the information of the
+    object like list of objects"""
     return dir(obj)
