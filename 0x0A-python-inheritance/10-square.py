f61ad7ed06a47cc5c20ed538716662d9b6e01474
JuanSebastianGB <3393@holbertonschool.com>
2021-10-11 19:13:50 +0000

    UPL: Uplaoding Task 10. Implementing class sqaure inherit form rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module of square"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Defining class square"""
+
+    def __init__(self, size) -> None:
+        """ Consturctor of the class Square"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ Returns the area of square """
+        return self.__size ** 2
