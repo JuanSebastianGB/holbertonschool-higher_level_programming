b3798ec8d146cf96fd6f3b7758ad517ad5671bda
JuanSebastianGB <3393@holbertonschool.com>
2021-10-12 03:46:05 +0000

    INFO: Adding documentation at the module

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+""" Module of the same class function"""
+
+
 def is_same_class(obj, a_class):
     """ returns True if the object is exactly an instance
     of the specified class ; otherwise False."""
