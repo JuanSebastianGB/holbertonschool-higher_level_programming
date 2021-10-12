0d55efd75819aba248b8bea7ad14c25b8189f84c
JuanSebastianGB <3393@holbertonschool.com>
2021-10-12 12:55:21 +0000

    UPL: Uploading tas 4, that returns a representation string form an object json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Module that has from_json_string function """
+from json import loads
+
+
+def from_json_string(my_str):
+    """ returns an object (Python data structure)
+    represented by a JSON string """
+    return loads(my_str)
