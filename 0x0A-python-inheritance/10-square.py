ea73a1a418d5937f288a18f83246c019e6eb9edc
JuanSebastianGB <3393@holbertonschool.com>
2021-10-12 05:56:32 +0000

    UPD: Moving super() at the end

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,9 +8,9 @@ class Square(Rectangle):
 
     def __init__(self, size) -> None:
         """ Consturctor of the class Square"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """ Returns the area of square """
