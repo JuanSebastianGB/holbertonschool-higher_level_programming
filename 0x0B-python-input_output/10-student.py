e11d0269feea6f4680f10b22ba5a6facb481d2e8
JuanSebastianGB <3393@holbertonschool.com>
2021-10-13 02:42:51 +0000

    UPL: Uploading task 10, implmeneting condition in the function to_json of the class Student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that contain class Student """
+
+
+class Student:
+    """ Definition of student """
+
+    def __init__(self, first_name, last_name, age) -> None:
+        """ Creation of student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Return the json representation of the class """
+        if isinstance(attrs, list) and all([type(el) is str for el in attrs]):
+            return {el: getattr(self, el) for el in attrs if hasattr(self, el)}
+        return self.__dict__
