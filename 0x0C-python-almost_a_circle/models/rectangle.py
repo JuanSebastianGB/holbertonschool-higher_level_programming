de36138bb080114f2321fab61a212b84fe126caa
JuanSebastianGB <3393@holbertonschool.com>
2021-10-15 02:20:29 +0000

    UPD: Including funciton to compute Area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,3 +70,14 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Area of the rectangle
+
+        Returns:
+            [int]: [Area calculated]
+        """
+        return self.height * self.width
+
+    def display(self):
+        print(("#" * self.width + '\n') * self.height, end="")
