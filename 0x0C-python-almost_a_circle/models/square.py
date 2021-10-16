00c9c8e3d6d3515e0f098b36ae16479557d7bebe
JuanSebastianGB <3393@holbertonschool.com>
2021-10-16 22:32:58 +0000

    UPD: Updating sqare classs, adding getter and setter metods for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,19 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Creating square"""
-        super().__init__(size, size, x, y, id)
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        self.size = size
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        """overloading __str__ method by returning
+        [Square] (<id>) <x>/<y> - <size>"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.width)
