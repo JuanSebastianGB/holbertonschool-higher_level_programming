76a2cc1715cb20fb31040ad088748807b910b76e
JuanSebastianGB <3393@holbertonschool.com>
2021-10-17 01:47:54 +0000

    UPD: Adding method from_json_string to get the objects information from a json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@
     Returns:
         [Class]: [Parent class to almost a circle project]
     """
+import json
 
 
 class Base:
@@ -27,10 +28,16 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        import json
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
