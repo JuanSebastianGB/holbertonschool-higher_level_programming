af66fe6c7eac3937f997f3fb288042f7ba727f66
JuanSebastianGB <3393@holbertonschool.com>
2021-10-18 15:10:48 +0000

    UPD: Uploading test cases for wrong values

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -87,6 +87,12 @@ class Test_rectangle(unittest.TestCase):
             Rectangle((1,), 5)
         with self.assertRaisesRegex(TypeError, "width must be an integer") as _:
             Rectangle(True, 5)
+        with self.assertRaisesRegex(TypeError, "width must be an integer") as _:
+            Rectangle({"1": "test"}, 5)
+        with self.assertRaisesRegex(TypeError, "width must be an integer") as _:
+            Rectangle({1, 2, 3}, 5)
+        with self.assertRaisesRegex(TypeError, "width must be an integer") as _:
+            Rectangle(complex(100), 5)
 
     def test_height_wrong_type(self):
         """[Testing Cases Width is not an integer, it must return the msj too]
@@ -101,6 +107,12 @@ class Test_rectangle(unittest.TestCase):
             Rectangle(5, (1,))
         with self.assertRaisesRegex(TypeError, "height must be an integer") as _:
             Rectangle(5, True)
+        with self.assertRaisesRegex(TypeError, "height must be an integer") as _:
+            Rectangle(5, {"1": "test"})
+        with self.assertRaisesRegex(TypeError, "height must be an integer") as _:
+            Rectangle(5, {1, 2, 3})
+        with self.assertRaisesRegex(TypeError, "height must be an integer") as _:
+            Rectangle(5, complex(100))
 
     def test_x_wrong_type(self):
         """[Testing Cases x is not an integer, it must return the msj too]
@@ -115,6 +127,12 @@ class Test_rectangle(unittest.TestCase):
             Rectangle(5, 10, (1,))
         with self.assertRaisesRegex(TypeError, "x must be an integer") as _:
             Rectangle(5, 10, True)
+        with self.assertRaisesRegex(TypeError, "x must be an integer") as _:
+            Rectangle(5, 10, {"1": "test"})
+        with self.assertRaisesRegex(TypeError, "x must be an integer") as _:
+            Rectangle(5, 10, {1, 2, 3})
+        with self.assertRaisesRegex(TypeError, "x must be an integer") as _:
+            Rectangle(5, 10, complex(100))
 
     def test_y_wrong_type(self):
         """[Testing Cases y is not an integer, it must return the msj too]
@@ -129,3 +147,37 @@ class Test_rectangle(unittest.TestCase):
             Rectangle(5, 10, 15, (1,))
         with self.assertRaisesRegex(TypeError, "y must be an integer") as _:
             Rectangle(5, 10, 15, True)
+        with self.assertRaisesRegex(TypeError, "y must be an integer") as _:
+            Rectangle(5, 10, 15, {"1": "test"})
+        with self.assertRaisesRegex(TypeError, "y must be an integer") as _:
+            Rectangle(5, 10, 15, {1, 2, 3})
+        with self.assertRaisesRegex(TypeError, "y must be an integer") as _:
+            Rectangle(5, 10, 15, complex(100))
+
+    def test_width_wrong_value(self):
+        """[Testing case width wrong value]
+        """
+        with self.assertRaisesRegex(ValueError, "width must be > 0") as _:
+            Rectangle(-10, 2, 3, 4, 5)
+        with self.assertRaisesRegex(ValueError, "width must be > 0") as _:
+            Rectangle(0, 2, 3, 4, 5)
+
+    def test_height_wrong_value(self):
+        """[Testing case height wrong value]
+        """
+        with self.assertRaisesRegex(ValueError, "height must be > 0") as _:
+            Rectangle(1, -2, 3, 4, 5)
+        with self.assertRaisesRegex(ValueError, "height must be > 0") as _:
+            Rectangle(1, 0, 3, 4, 5)
+
+    def test_x_wrong_value(self):
+        """[Testing case x wrong value]
+        """
+        with self.assertRaisesRegex(ValueError, "x must be >= 0") as _:
+            Rectangle(1, 2, -3, 4, 5)
+
+    def test_y_wrong_value(self):
+        """[Testing case y wrong value]
+        """
+        with self.assertRaisesRegex(ValueError, "y must be >= 0") as _:
+            Rectangle(1, 2, 3, -4, 5)
