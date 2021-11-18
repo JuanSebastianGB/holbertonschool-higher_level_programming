adf58f0f03aa6b0370eb12e1797c57ade9e38c5f
JuanSebastianGB <3393@holbertonschool.com>
2021-11-18 17:02:40 +0000

    UPL: Uploading task 19 Rotten tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,17 @@
+-- Import the database hbtn_0d_tvshows_rate dump to your MySQL server: download
+
+-- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in descending order by the rating
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT
+    title,
+    SUM(rate) as rating
+FROM
+    `tv_show_ratings`
+INNER JOIN tv_shows ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY
+    title
+    order by rating desc;
