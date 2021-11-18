afe5e7aba88c2efaa57824a40719365402bda11e
JuanSebastianGB <3393@holbertonschool.com>
2021-11-18 03:37:37 +0000

    FIX: Updating sql to accomplish task requirement

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -14,10 +14,4 @@ FROM
 LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE
-    tv_show_genres.show_id =(
-    SELECT
-        tv_shows.id
-    FROM
-        tv_shows
-    WHERE
-        tv_shows.title = "Dexter"
+    tv_shows.title = "Dexter";
