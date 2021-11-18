91fcec2b7bf6704057b9d80399af950f236ca568
JuanSebastianGB <3393@holbertonschool.com>
2021-11-18 03:47:00 +0000

    FIX: Changing the column name

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -8,12 +8,12 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT
-    tv_shows.title AS name
+    tv_shows.title
 FROM
     tv_show_genres
 LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE
     tv_genres.name = "Comedy"
-ORDER BY name ASC
+ORDER BY tv_shows.title ASC
     ;
