5dc77fa2d8dd7ad39d8616eb1032cd7fadda0d32
JuanSebastianGB <3393@holbertonschool.com>
2021-12-14 02:19:44 +0000

    handeling data type outside the function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const input = process.argv;
 const getFactorial = (input) => {
-  input = parseInt(input);
   return input === 0 || isNaN(input) ? 1 : input * getFactorial(input - 1);
 };
-console.log(getFactorial(input[2]));
+console.log(getFactorial(parseInt(input[2])));
