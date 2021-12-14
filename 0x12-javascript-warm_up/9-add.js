673fdec95f3aec98943dede6ffedc36ec55b348a
JuanSebastianGB <3393@holbertonschool.com>
2021-12-14 02:02:33 +0000

    FIX: Fixing NaN output

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const inputs = process.argv;
 const add = (inputs) => {
-  if (inputs.length <= 3) {
-    console.log(NaN);
-    return;
+  if (inputs.length <= 3 || isNaN(inputs[2]) || isNaN(inputs[3])) {
+    return NaN;
   }
   return parseInt(inputs[2]) + parseInt(inputs[3]);
 };
