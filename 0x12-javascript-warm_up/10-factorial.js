0a805d594a6e42c6806e6cdf134ddaabc607f2d7
JuanSebastianGB <3393@holbertonschool.com>
2021-12-14 02:17:11 +0000

    FIX: COnverting to ingetegers

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const input = process.argv;
 const getFactorial = (input) => {
+  input = parseInt(input);
   return input === 0 || isNaN(input) ? 1 : input * getFactorial(input - 1);
 };
 console.log(getFactorial(input[2]));
