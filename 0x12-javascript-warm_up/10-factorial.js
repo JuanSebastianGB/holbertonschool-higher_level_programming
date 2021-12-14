91364fedfb86042a93b362820c9bac74eb70c4cd
JuanSebastianGB <3393@holbertonschool.com>
2021-12-14 02:25:41 +0000

    FIX: Adding an space and removing unnecesary semicolon

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const input = process.argv;
-function getFactorial(input)
-{
+function getFactorial (input) {
   return input === 0 || isNaN(input) ? 1 : input * getFactorial(input - 1);
-};
+}
 console.log(getFactorial(parseInt(input[2])));
