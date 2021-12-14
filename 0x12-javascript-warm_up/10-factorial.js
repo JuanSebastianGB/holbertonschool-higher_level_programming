cc4901f211cebcfeee6e491c4354b5dbebd13671
JuanSebastianGB <3393@holbertonschool.com>
2021-12-14 02:22:47 +0000

    upd: Changing narrow function by normal function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const input = process.argv;
-const getFactorial = (input) => {
+function getFactorial(input)
+{
   return input === 0 || isNaN(input) ? 1 : input * getFactorial(input - 1);
 };
 console.log(getFactorial(parseInt(input[2])));
