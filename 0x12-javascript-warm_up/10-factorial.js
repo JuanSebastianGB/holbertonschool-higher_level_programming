3967ff37871a3536753838efc895b9f5da0c82cc
JuanSebastianGB <3393@holbertonschool.com>
2021-12-14 02:15:34 +0000

    UPL: Uploading task 10, implementing recursive function to get the factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const input = process.argv;
+const getFactorial = (input) => {
+  return input === 0 || isNaN(input) ? 1 : input * getFactorial(input - 1);
+};
+console.log(getFactorial(input[2]));
