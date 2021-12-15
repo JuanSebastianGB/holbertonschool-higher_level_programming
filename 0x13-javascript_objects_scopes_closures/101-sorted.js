20746def6675ae436996ea15b987c553346372a3
JuanSebastianGB <3393@holbertonschool.com>
2021-12-15 04:07:34 +0000

    UPL: Uploading task 12. Sorting occurrences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+let value;
+for (const key in dict) {
+  value = dict[key];
+  if (newDict[value] !== undefined) {
+    newDict[value].push(key);
+  } else {
+    newDict[value] = [];
+  }
+}
+console.log(newDict);
