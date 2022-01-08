90efd00e95b59ffdf2edead941a8729f63aa710a
JuanSebastianGB <3393@holbertonschool.com>
2022-01-08 03:19:00 +0000

    INFO: Fixing content task description

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-# Display only body of a 200 status code response
+# The size must be displayed in bytes
 # Mandatory to use curl
 
 curl -s "$1" | wc -c
