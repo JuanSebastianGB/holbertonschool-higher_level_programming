fb01491ea3985cd9c95a255cd3957460a0a0adfe
JuanSebastianGB <3393@holbertonschool.com>
2022-01-09 14:30:04 +0000

    UPD: Testing without HTTPAuth module

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,9 +5,8 @@ and uses the GitHub API to display the id
 """
 from sys import argv
 import requests
-from requests.auth import HTTPBasicAuth
 
 if __name__ == '__main__':
-    response = requests.get('https://api.github.com/users/{}'.format(argv[1]),
-                            auth=HTTPBasicAuth(argv[1], argv[2])).json()
+    auth = (argv[1], argv[2])
+    response = requests.get('https://api.github.com/user', auth=auth).json()
     print(response.get('id'))
