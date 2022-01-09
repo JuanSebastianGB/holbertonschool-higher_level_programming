13cc4417a6bc156a6e0c5aa68b823a602bf27502
JuanSebastianGB <3393@holbertonschool.com>
2022-01-09 15:43:09 +0000

    CHCECKER: Testing catching error

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,10 +14,13 @@ if __name__ == '__main__':
     url = 'https://api.github.com/repos/{}/{}/commits'\
         .format(sys.argv[2], sys.argv[1])
     commits = requests.get(url).json()
-    for i in range(10):
-        print('{}: {}'.format(
-            commits[i].get('sha'),
-            commits[i].get('commit')
-            .get('author')
-            .get('name')
-        ))
+    try:
+        for i in range(10):
+            print('{}: {}'.format(
+                commits[i].get('sha'),
+                commits[i].get('commit')
+                .get('author')
+                .get('name')
+            ))
+    except IndexError:
+        pass
