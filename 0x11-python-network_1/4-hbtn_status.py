b01d7298d90adc1d30067e52276d826c50c496a8
JuanSebastianGB <3393@holbertonschool.com>
2022-01-09 05:30:56 +0000

    FIX: Fixing docstring position

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import requests
 """ Python script that fetches https://intranet.hbtn.io/status
 Used the package requests
 )"""
+import requests
 
 if __name__ == '__main__':
     response = requests.get('https://intranet.hbtn.io/status')
