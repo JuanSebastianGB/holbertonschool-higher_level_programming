e980c6bdd41ad9aa2780b3f03cb3d2f9206ae4ba
JuanSebastianGB <3393@holbertonschool.com>
2022-01-09 04:49:49 +0000

    UPL: Uploading task 3. Catching errors

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to
+the URL and displays the body of the response (decoded in utf-8)."""
+import sys
+import urllib.request as request
+import urllib.error as error
+
+if __name__ == '__main__':
+    req = request.Request(sys.argv[1])
+    try:
+        with request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print(f'Error code: {err.code}')
