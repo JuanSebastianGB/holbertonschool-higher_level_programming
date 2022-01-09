1d1e63603549038e92c469b6930d61041a9749fe
JuanSebastianGB <3393@holbertonschool.com>
2022-01-09 03:50:04 +0000

    UPL: Uploading task 1, to get an specifica information inside a header response

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id variable found in the header
+of the response."""
+import sys
+import urllib.request as imported
+
+if __name__ == '__main__':
+    request = imported.Request(sys.argv[1])
+    with imported.urlopen(request) as response:
+        headers = response.headers
+        print(dict(headers)['X-Request-Id'])
