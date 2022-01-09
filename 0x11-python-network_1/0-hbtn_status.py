a119b0591f65c9e459ef82d5f476486205906b5b
JuanSebastianGB <3393@holbertonschool.com>
2022-01-09 03:26:24 +0000

    FIX: Accomplish requirement of main function

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,11 @@
 """Python script that fetches https://intranet.hbtn.io/status"""
 import urllib.request as imported
 
-with imported.urlopen('https://intranet.hbtn.io/status') as response:
-    content = response.read()
-    print('Body response:')
-    print('    - type: {}'.format(type(content)))
-    print('    - content: {}'.format(content))
-    print('    - utf8 content: {}'.format(content.decode('utf-8')))
+if __name__ == '__main__':
+
+    with imported.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print('Body response:')
+        print('    - type: {}'.format(type(content)))
+        print('    - content: {}'.format(content))
+        print('    - utf8 content: {}'.format(content.decode('utf-8')))
