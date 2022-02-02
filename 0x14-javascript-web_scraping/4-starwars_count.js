49f91bca1a76dc0fdad1766759b849561a30c368
JuanSebastianGB <3393@holbertonschool.com>
2022-02-02 03:18:51 +0000

    UPL: Uploading task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+const FilterFunction = (data) => {
+  const result = data.filter(characters => {
+    return characters === 'https://swapi-api.hbtn.io/api/people/18/';
+  });
+  return result.length;
+};
+
+request(url, (err, data, body) => {
+  const result = JSON.parse(body).results;
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  let count = 0;
+  result.map(dictionary => {
+    if (FilterFunction(dictionary.characters) > 0) { count++; }
+    return count;
+  });
+  console.log(count);
+});
