a237d41abff38055e333c6689ecbd843a32712b6
JuanSebastianGB <3393@holbertonschool.com>
2022-02-15 05:03:55 +0000

    STYLE: Format documents

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -6,7 +6,7 @@ const fetchResponse = async () => {
   const url = 'https://www.fourtonfish.com/hellosalut/?';
   const response = $.get(url + $.param({ lang: $('input#language_code').val() }));
   return response;
-}
+};
 const translate = async () => {
   $('input#btn_translate').click(async () => {
     const response = await fetchResponse();
@@ -20,8 +20,7 @@ const applyEnter = async () => {
 const translateByEnter = () => {
   $('input#language_code').focus(() => {
     $(this).keydown((e) => {
-      if (e.keyCode === 13)
-        applyEnter();
+      if (e.keyCode === 13) { applyEnter(); }
     });
   });
-}
+};
