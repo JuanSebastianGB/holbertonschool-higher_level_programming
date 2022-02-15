7a5dd7119805006475ad03758f82d4b51ae01103
JuanSebastianGB <3393@holbertonschool.com>
2022-02-15 04:35:24 +0000

    UPL: Uploading task 12

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,10 @@
+$('document').ready(function () {
+  translate();
+});
+const translate = async () => {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $('input#btn_translate').click(async () => {
+    const response = await $.get(url + $.param({ lang: $('input#language_code').val() }));
+    $('div#hello').html(response.hello);
+  });
+};
