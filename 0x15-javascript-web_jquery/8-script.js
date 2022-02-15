840c9bd1217553dde58a338359ae8c96fd4dc890
JuanSebastianGB <3393@holbertonschool.com>
2022-02-15 02:54:42 +0000

    UPL: Uploading task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  printTitles();
+};
+const getMovies = async () => {
+  const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+  const movies = $.get(url);
+  return movies;
+};
+const printTitles = async () => {
+  const movies = (await getMovies()).results;
+  movies.map((movie, index) => {
+    $('ul#list_movies').append(`<li>${movie.title}</li>`);
+    return true;
+  });
+};
